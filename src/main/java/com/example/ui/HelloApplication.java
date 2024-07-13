88a458cc9082a5238171214b91d7479928b5fb0b
son <sonpredator321@gmail.com>
2024-07-14 01:38:11 +0700

    -Done mkdir

--- src/main/java/com/example/ui/HelloApplication.java
+++ src/main/java/com/example/ui/HelloApplication.java
@@ -20,7 +20,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 650);
         stage.setTitle("FilTran Client");
         stage.setScene(scene);
         stage.show();
