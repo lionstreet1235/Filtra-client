f039820a5ed54e0da4644f06bfa8508750c6fde3
son <sonpredator321@gmail.com>
2024-07-18 17:55:52 +0700

    -done share file

--- src/main/java/com/example/ui/controller/ShareViewController.java
+++ src/main/java/com/example/ui/controller/ShareViewController.java
@@ -1,9 +1,53 @@
 package com.example.ui.controller;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import com.example.ui.model.SharedDataModel;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
+import static com.example.ui.controller.ClientController.out;
+
+
 
 public class ShareViewController {
     public TextField emailField;
     public Button otpBtn;
+    public CheckBox checkBoxFull;
+    public CheckBox checkBoxRead;
+    public CheckBox checkBoxWrite;
+    private String Message;
+
+    @FXML
+    private void ShareFile(){
+        try{
+            String selectedFile = SharedDataModel.getSelectedFileForShare();
+            String email = emailField.getText();
+            out.println("shr -e "+email+" -p "+Message+" -f "+selectedFile);
+            Platform.runLater(() -> {showAlert("Share status", selectedFile+" has been shared to "+ email);});
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void setFullMessage(){
+        Message = "FULL";
+    }
+    @FXML
+    private void setReadMessage(){
+        Message = "READ";
+    }
+    @FXML
+    private void setWriteMessage(){
+        Message = "WRITE";
+    }
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
